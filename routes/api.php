7683ca6066c817cd334661e99c5111270a55d243
Caesar Ali L <caesaralilamondo@gmail.com>
2018-09-08 14:21:20 +0800

    .

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Http\Request;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::get('/departures', function () {
     $flights = App\Flight::where('type', 'departure')->whereHas('schedules', function ($query) {
